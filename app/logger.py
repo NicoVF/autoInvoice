1c4835f1379bb4c4899d54ef70a27e29606f2b93
nicovf11 <nicoveliz11@hotmail.com>
2025-10-14 04:15:02 -0300

    loggerApp

--- app/logger.py
+++ app/logger.py
@@ -2,6 +2,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(name)s - %(message)s")
 
+loggerApp = logging.getLogger("App")
 loggerWhappi = logging.getLogger("Whappi")
 loggerCloudVision = logging.getLogger("CloudVision")
 loggerGSpreadsheet = logging.getLogger("GSpreadsheet")
