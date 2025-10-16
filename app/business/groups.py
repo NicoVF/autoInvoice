fa385a24c2ae698a6ded4d10c8281ea7fa9150e0
nicovf11 <nicoveliz11@hotmail.com>
2025-10-16 01:28:55 -0300

    add get_group_balance
    add get_group_expected_alias
    add get_group_expected_name

--- app/business/groups.py
+++ app/business/groups.py
@@ -1,7 +1,8 @@
 import os
 import threading
 from app.logger import loggerApp
-from app.services.spreadsheet import fetch_rows_from_sheet
+from app.services.spreadsheet import fetch_rows_from_sheet, sheet_exists
+
 
 ALLOWED_GROUPS_CACHE = {"groups": []}
 CACHE_LOCK = threading.Lock()
@@ -12,6 +13,10 @@ SPREADSHEET_GROUPS_NAMES_COLUMN = int(os.environ["SPREADSHEET_GROUPS_NAMES_COLUM
 SPREADSHEET_GROUPS_ACTIVE_COLUMN = int(os.environ["SPREADSHEET_GROUPS_ACTIVE_COLUMN"])
 SPREADSHEET_GROUPS_COMMISSION = int(os.environ["SPREADSHEET_GROUPS_COMMISSION"])
 SPREADSHEET_GROUPS_EXPECTED_CBU = int(os.environ["SPREADSHEET_GROUPS_EXPECTED_CBU"])
+SPREADSHEET_GROUPS_EXPECTED_ALIAS = int(os.environ["SPREADSHEET_GROUPS_EXPECTED_ALIAS"])
+SPREADSHEET_GROUPS_EXPECTED_NAME = int(os.environ["SPREADSHEET_GROUPS_EXPECTED_NAME"])
+SPREADSHEET_COLUMN_GROUPS_TODAY_AVAILABLE_BALANCE = int(os.environ["SPREADSHEET_COLUMN_GROUPS_TODAY_AVAILABLE_BALANCE"])
+SPREADSHEET_ROW_GROUPS_TODAY_AVAILABLE_BALANCE = int(os.environ["SPREADSHEET_ROW_GROUPS_TODAY_AVAILABLE_BALANCE"])
 
 
 def update_allowed_groups():
@@ -21,9 +26,11 @@ def update_allowed_groups():
                 SPREADSHEET_GROUPS_ACTIVE_COLUMN,
                 SPREADSHEET_GROUPS_COMMISSION,
                 SPREADSHEET_GROUPS_EXPECTED_CBU,
+                SPREADSHEET_GROUPS_EXPECTED_ALIAS,
+                SPREADSHEET_GROUPS_EXPECTED_NAME
             ], has_headers=True)
         groups = []
-        for name, is_active, commission, expected_cbu in rows:
+        for name, is_active, commission, expected_cbu, expected_alias, expected_name in rows:
             name = name.strip()
             if not name or is_active.upper() != "TRUE":
                 continue
@@ -36,7 +43,9 @@ def update_allowed_groups():
             groups.append({
                 "name": name,
                 "commission": commission_value,
-                "expected_cbu": expected_cbu
+                "expected_cbu": expected_cbu,
+                "expected_alias": expected_alias,
+                "expected_name": expected_name
             })
         with CACHE_LOCK:
             ALLOWED_GROUPS_CACHE["groups"].clear()
@@ -46,6 +55,31 @@ def update_allowed_groups():
     except Exception as e:
         loggerApp.exception(f"❌ Error updating groups: {e}")
         return []
+
+
+def get_group_balance(group_name):
+    try:
+        if not sheet_exists(SPREADSHEET_ID, group_name):
+            loggerApp.warning(f"Sheet not found for group '{group_name}'")
+            raise Exception(f"Sheet not found for group '{group_name}'")
+        column_cell = SPREADSHEET_COLUMN_GROUPS_TODAY_AVAILABLE_BALANCE
+        row_cell = SPREADSHEET_ROW_GROUPS_TODAY_AVAILABLE_BALANCE
+        values = fetch_rows_from_sheet(SPREADSHEET_ID, group_name, [column_cell], has_headers=False)
+        if not values or not values[0] or not values[0][0]:
+            loggerApp.warning(f"No balance found in {group_name}! {chr(64 + column_cell)}{row_cell}")
+            return None
+        raw_value = str(values[0][row_cell - 1]).strip()
+        try:
+            balance = raw_value.replace(".", "X").replace(",", ".").replace("X", ",").replace("$", "").strip()
+            balance = balance if float(balance.replace(".", "").replace(",", ".").strip()) > 0 else "0,00"          ## Si es negativo, enviamos 0
+        except ValueError:
+            loggerApp.warning(f"Invalid balance format in {group_name}!{column_cell}{row_cell}: '{raw_value}'")
+            return None
+        loggerApp.info(f"Balance for '{group_name}': {balance}")
+        return balance
+    except Exception as e:
+        loggerApp.error(f"Error fetching balance for '{group_name}': {e}")
+        return None
 
 
 def get_group_commission(chat_name):
@@ -59,4 +93,18 @@ def get_group_expected_cbu(chat_name):
     for g in ALLOWED_GROUPS_CACHE["groups"]:
         if g["name"] == chat_name:
             return g["expected_cbu"]
+    return None
+
+
+def get_group_expected_alias(chat_name):
+    for g in ALLOWED_GROUPS_CACHE["groups"]:
+        if g["name"] == chat_name:
+            return g["expected_alias"]
+    return None
+
+
+def get_group_expected_name(chat_name):
+    for g in ALLOWED_GROUPS_CACHE["groups"]:
+        if g["name"] == chat_name:
+            return g["expected_name"]
     return None
